8d27efedb05fadfa1670217302cf3a5d3d43d204
meow <nasty0701@gmail.com>
2021-05-18 22:07:05 +0300

    Electronic digital signature RSA

---
+++ TI3/TI3/Hash.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Numerics;
+
+namespace TI3
+{
+    public class Hash
+    {
+        private long p;
+        private long q;
+        private long h;
+
+        public Hash(long p, long q, long h = 100)
+        {
+            this.p = p;
+            this.q = q;
+            this.h = h;
+        }
+
+        public long HashFunction(string message)
+        {
+            long hash = this.h;
+            long n = this.p * this.q;
+
+            foreach (char c in message)
+            {
+                hash = (long)BigInteger.ModPow(hash + (int)c, 2, n);
+            }
+
+            return hash;
+        }
+    }
+}
